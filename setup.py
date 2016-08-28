7cb2203c4402adf4edcb0a4b375d9b22ff3d0692
Christian Schulze <christian.schulze@mni.thm.de>
2016-08-28 10:48:10 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pythia',
+    version='0.0.1',
+    author='Christian Schulze',
+    author_email='chris@andinfinity.de',
+    url='https://github.com/ChristianSch/PyPythia'
+)
