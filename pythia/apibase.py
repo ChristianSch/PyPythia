ccaab36ce3de695cbea097a8729c47a6cbb15e01
Christian Schulze <christian.schulze@mni.thm.de>
2016-08-28 10:40:37 +0200

    Renamed file; changed doc to be valid rst; fixed #4; added requests error handling

--- pythia/apibase.py
+++ pythia/apibase.py
@@ -5,21 +5,26 @@ import os
 class ApiBase(object):
     """
     This class provides the basis for every remote entity. The resource simply
-    specifies its `attributes` and path, as well as provides setters for the
+    specifies its ``attributes`` and path, as well as provides setters for the
     attributes.
 
     It might also provide custom methods as well as a custom initiator. It is
     mandatory that the inheriting entity calls this class's init method as
-    well:
+    well::
 
-    ```
-    ApiBase.__init__(self, api_base, _id=_id)
-    ```
+        ApiBase.__init__(self, api_base, _id=_id)
     """
     _id = None
     api_base = None
 
     def __init__(self, api_base, _id=None):
+        """
+        Creates a new entitiy. If ``_id`` is provided, an already existing
+        remote entity is fetched, otherwise a new one is created.
+
+        :param api_base: URI to the Pythia API instance
+        :param _id: if given, attempts to fetch an already existing model.
+        """
         self.api_base = api_base
 
         # init attributes to prevent "no such attribute" errors.
@@ -40,7 +45,10 @@ class ApiBase(object):
         returned to synch the object with the remote.
         """
         res = requests.get(self._resource_path())
-        d = res.json()
+        res.raise_for_status
+        data = res.json()
+
+        assert '_id' in data, 'Invalid _id'
 
         for attribute in self._attributes:
             if attribute.startswith('_'):
@@ -60,12 +68,14 @@ class ApiBase(object):
     def _create_resource(self):
         """
         Creates the resource on the remote. The remote returns a dict with
-        the initial/sent fields, which, if in `_attributes` listed, are set
+        the initial/sent fields, which, if in ``_attributes`` listed, are set
         for the current object, thus synched with the remote.
         """
         res = requests.post(self._resource_path())
+        res.raise_for_status
         data = res.json()
 
+        assert '_id' in data, 'Remote entity could not be registered'
         self._id = data['_id']
 
         for attribute in self._attributes:
@@ -81,10 +91,10 @@ class ApiBase(object):
         """
         Joins an arbritary number of URI fragments
 
-        @param One or more URI fragments that ought to be joined with a single
+        :param: One or more URI fragments that ought to be joined with a single
         slash in between
 
-        @returns Joined URI
+        :return: Joined URI
         """
         return os.path.join(*args)
 
